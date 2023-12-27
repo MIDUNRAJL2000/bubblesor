fd13f2396c7c23859586ac160d34c1c392445e34
MIDUNRAJL2000 <midun.raj@edstem.com>
2023-12-27 15:29:59 +0530

    commited

--- bubb/src/components/BinarySearch.js
+++ bubb/src/components/BinarySearch.js
@@ -1,5 +1,5 @@
 // import React, { useState } from "react";
-// import BinaryVisualiztion from "./BinaryVisualization";
+// import BinaryVisualization from "./BinaryVisualization";
 
 // function BinarySearch() {
 //   const [inputArray, setInputArray] = useState("");
@@ -7,7 +7,10 @@
 //   const [resultIndex, setResultIndex] = useState(null);
 //   const [searchState, setSearchState] = useState(null);
 
+//   const [executionTime, setExecutionTime] = useState(0);
+
 //   const binarySearch = (arr, target) => {
+//     const startTime = performance.now();
 //     let leftIndex = 0;
 //     let rightIndex = arr.length - 1;
 
@@ -35,7 +38,9 @@
 
 //     setResultIndex(-1);
 //     setSearchState(searchSteps);
-//     return -1;
+//     const endTime = performance.now();
+//     console.log(endTime - startTime);
+//     setExecutionTime(endTime - startTime);
 //   };
 
 //   const onSearch = () => {
@@ -68,12 +73,16 @@
 //       <button onClick={onSearch}>Search the element</button>
 //       <div>
 //         <span> {inputArray && <h4>Array : {inputArray} </h4>} </span>
-//         {searchState && <BinaryVisualiztion searchState={searchState} />}
+//         {searchState && <BinaryVisualization searchState={searchState} />}
 //         {resultIndex != null ? (
 //           resultIndex !== -1 ? (
-//             <p className="bold">Element found at index: {resultIndex}</p>
+//             <>
+//               <p className="found">Element found at index: {resultIndex}</p>
+
+//               <p>Execution Time: {executionTime.toFixed(4)} milliseconds</p>
+//             </>
 //           ) : (
-//             <p>There is no element found in the array</p>
+//             <p className="not-found">There is no element found in the array</p>
 //           )
 //         ) : null}
 //       </div>
@@ -83,36 +92,46 @@
 
 // export default BinarySearch;
 
-import React, { useState } from "react";
-import BinaryVisualization from "./BinaryVisualization";
+import React, { useState, useEffect } from "react";
+
+function ArrayVisualization({ array, target, currentIndex }) {
+  return (
+    <div className="array-visualization">
+      {array.map((value, index) => (
+        <div
+          key={index}
+          className={`array-element ${index === currentIndex ? "current" : ""}`}
+        >
+          {value}
+          {index === currentIndex && <div className="arrow"></div>}
+        </div>
+      ))}
+      {currentIndex !== null && currentIndex === array.length - 1 && (
+        <div className="search-element green">{target}</div>
+      )}
+    </div>
+  );
+}
 
 function BinarySearch() {
   const [inputArray, setInputArray] = useState("");
   const [searchElement, setSearchElement] = useState("");
   const [resultIndex, setResultIndex] = useState(null);
-  const [searchState, setSearchState] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
 
-  const [executionTime, setExecutionTime] = useState(0);
+  useEffect(() => {
+    setCurrentIndex(null);
+  }, [inputArray]);
 
   const binarySearch = (arr, target) => {
-    const startTime = performance.now();
     let leftIndex = 0;
     let rightIndex = arr.length - 1;
 
-    const searchSteps = [];
-
     while (leftIndex <= rightIndex) {
       const mid = Math.floor((leftIndex + rightIndex) / 2);
-
-      searchSteps.push({
-        leftIndex,
-        rightIndex,
-        mid,
-      });
+      setCurrentIndex(mid);
 
       if (arr[mid] === target) {
-        setResultIndex(mid);
-        setSearchState(searchSteps);
         return mid;
       } else if (arr[mid] < target) {
         leftIndex = mid + 1;
@@ -120,24 +139,24 @@ function BinarySearch() {
         rightIndex = mid - 1;
       }
     }
-
-    setResultIndex(-1);
-    setSearchState(searchSteps);
-    const endTime = performance.now();
-    console.log(endTime - startTime);
-    setExecutionTime(endTime - startTime);
+    return -1;
   };
 
   const onSearch = () => {
     const array = inputArray.split(",").map((num) => parseInt(num.trim(), 10));
-    const index = binarySearch(array, parseInt(searchElement, 10));
-    setResultIndex(index);
+    const target = parseInt(searchElement, 10);
+    if (!isNaN(target)) {
+      const index = binarySearch(array, target);
+      setResultIndex(index);
+    } else {
+      setResultIndex(null);
+    }
   };
 
   return (
-    <div className="heading">
+    <div className="binary-search-container">
       <h2>Binary search app</h2>
-      <div>
+      <div className="input-container">
         <label htmlFor="inputArray">Enter the array</label>
         <input
           type="text"
@@ -146,7 +165,7 @@ function BinarySearch() {
           onChange={(e) => setInputArray(e.target.value)}
         />
       </div>
-      <div>
+      <div className="input-container">
         <label htmlFor="searchElement">Enter the value to search</label>
         <input
           type="text"
@@ -156,18 +175,23 @@ function BinarySearch() {
         />
       </div>
       <button onClick={onSearch}>Search the element</button>
-      <div>
-        <span> {inputArray && <h4>Array : {inputArray} </h4>} </span>
-        {searchState && <BinaryVisualization searchState={searchState} />}
+      <div className="visualization-container">
+        <span>
+          {inputArray && (
+            <ArrayVisualization
+              array={inputArray
+                .split(",")
+                .map((num) => parseInt(num.trim(), 10))}
+              target={parseInt(searchElement, 10)}
+              currentIndex={currentIndex}
+            />
+          )}
+        </span>
         {resultIndex != null ? (
           resultIndex !== -1 ? (
-            <>
-              <p className="found">Element found at index: {resultIndex}</p>
-
-              <p>Execution Time: {executionTime.toFixed(4)} milliseconds</p>
-            </>
+            <p className="bold">Element found at index: {resultIndex}</p>
           ) : (
-            <p className="not-found">There is no element found in the array</p>
+            <p>There is no element found in the array</p>
           )
         ) : null}
       </div>
--- bubb/src/components/BinaryVisualization.js
+++ bubb/src/components/BinaryVisualization.js
@@ -1,20 +1,3 @@
-// import React from "react";
-
-// function BinaryVisualiztion({ searchState }) {
-//   return (
-//     <div>
-//       {searchState.map((step, index) => (
-//         <p key={index}>
-//           Iteration {index + 1}: Left Index={step.leftIndex}, Right Index=
-//           {step.rightIndex}, Mid Value={step.mid}
-//         </p>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default BinaryVisualiztion;
-
 import React from "react";
 
 function BinaryVisualization({ searchState }) {
